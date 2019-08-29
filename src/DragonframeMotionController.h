8b2e8f4d4955b94b611c797f2c2433efb9edf4ae
karol-stola <karolstola@gmail.com>
2019-08-29 22:13:23 +0200

    motor status query handling

--- src/DragonframeMotionController.h
+++ src/DragonframeMotionController.h
@@ -7,16 +7,26 @@ class DragonframeMotionController
 {
 public:
     void Initialize(class DragonframeDevice * newDragonframeDevice);
-    void HandleMessage(const String & message);
+    void ParseInput(const String & input);
+
     virtual ~DragonframeMotionController();
 
 private:
+    void SendMessage(String & message);
+
     bool IsMessageHi(String & message);
+    bool IsMessageQueryAreMotorsMoving(String & message);
+
+    void HandleMessageHi(String & message);
+    void HandleMessageQueryAreMotorsMoving(String & message);
+
     void ParseMessage(String & message);
+    bool IsEndOfMessage(const String & message, int characterIndex);
+    bool IsOmittedCharacter(char character);
 
     const char * messageHi = "hi";
-    const char * messageCheckMotorMovingStatus = "ms";
-    const char * messageMoveMotor = "mm";
+    const char * messageQueryAreMotorsMoving = "ms";
+    const char * messageMoveMotorTo = "mm";
     const char * messageQueryMotorPosition = "mp";
     const char * messageStopMotor = "sm";
     const char * messageStopAllMotors = "sa";
@@ -26,6 +36,7 @@ private:
     const char * messageZeroMotorPosition = "zm";
     const char * messageSetMotorPosition = "np";
     const char * messagePerformMotionBlur = "bf";
+    const char * messageEnding = "\r\n";
 
     class DragonframeDevice * dragonframeDevice = nullptr;
 };
--- src/source/DragonframeMotionController.cpp
+++ src/source/DragonframeMotionController.cpp
@@ -7,35 +7,50 @@ void DragonframeMotionController::Initialize(DragonframeDevice * newDragonframeD
     dragonframeDevice->BindAsMessageHandler(*this);
 }
 
-void DragonframeMotionController::HandleMessage(const String & message)
+void DragonframeMotionController::ParseInput(const String & input)
 {
-    String currentMessage;
+    String message;
     
-    for(int i = 0; i < message.length(); i++)
+    for(int i = 0; i < input.length(); i++)
     {
-        if(message[i] == '\n' && i > 0 && message[i-1] == '\r')
+        if(IsEndOfMessage(input, i))
         {
-            ParseMessage(currentMessage);
-            currentMessage = "";
+            ParseMessage(message);
+            message = "";
         }
-        else if (message[i] != '\r')
+        else if (!IsOmittedCharacter(input[i]))
         {
-            currentMessage += message[i];
+            message += input[i];
         }    
     }
 }
+
+bool DragonframeMotionController::IsEndOfMessage(const String & message, int characterIndex)
+{
+    return message[characterIndex] == '\n' && characterIndex > 0 && message[characterIndex-1] == '\r';
+}
+
+bool DragonframeMotionController::IsOmittedCharacter(char character)
+{
+    return character == '\r';
+}
+
+void DragonframeMotionController::SendMessage(String & message)
+{
+    Serial.println(message);
+    dragonframeDevice->SendMessage(message);
+}
+
 
 void DragonframeMotionController::ParseMessage(String & message)
 {
     if(IsMessageHi(message))
     {
-        auto replyHi =
-            String(messageHi)
-            + " " + dragonframeDevice->GetProtocolMajorVersion()
-            + " " + dragonframeDevice->GetNumberOfAxes()
-            + " " + dragonframeDevice->GetProtocolFullVersion()
-            + "\r\n" ;
-        dragonframeDevice->SendMessage(replyHi);
+        HandleMessageHi(message);
+    }
+    else if (IsMessageQueryAreMotorsMoving(message))
+    {
+        HandleMessageQueryAreMotorsMoving(message);
     }
 }
 
@@ -44,6 +59,36 @@ bool DragonframeMotionController::IsMessageHi(String & message)
     return message.startsWith(messageHi);
 }
 
+bool DragonframeMotionController::IsMessageQueryAreMotorsMoving(String & message)
+{
+    return message.startsWith(messageQueryAreMotorsMoving);
+}
+
+void DragonframeMotionController::HandleMessageHi(String & message)
+{
+    static String reply =
+        String(messageHi)
+        + " " + dragonframeDevice->GetProtocolMajorVersion()
+        + " " + dragonframeDevice->GetNumberOfAxes()
+        + " " + dragonframeDevice->GetProtocolFullVersion()
+        + messageEnding;
+
+    SendMessage(reply);
+}
+
+void DragonframeMotionController::HandleMessageQueryAreMotorsMoving(String & message)
+{
+    auto reply = String(messageQueryAreMotorsMoving) + ' ';
+
+    for(int i = 0; i < dragonframeDevice->GetNumberOfAxes(); i++)
+    {
+        reply += dragonframeDevice->GetIsMotorMoving(i) ? '1' : '0';
+    }
+
+    reply += messageEnding;
+
+    SendMessage(reply);
+}
 
 DragonframeMotionController::~DragonframeMotionController()
 {
